ed881689354c693a23fa472d7b5d7631ff84f379
ben weber <weberbencs@gmail.com>
2021-04-12 13:03:00 -0400

    h

---
+++ DS-A/Data Structures/Arrays/Basic Array/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Method 1: intialization 
+            // Pro: most space controlled
+            // Con: requires knowing max size initally
+            // Follows path: datatype[] arrayName = new datatype[size];
+            int[] declaredArray = new int[5];
+            int i, j;
+
+            // Adding elements to the declared array
+            for ( i = 0; i <= 5; i++)
+            {
+                declaredArray[i] = i;
+            }
+
+            for (j = 0; j <= 5; j++)
+            {
+                Console.WriteLine($"Elecment[{0}] = {1}", j, declaredArray[j]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
